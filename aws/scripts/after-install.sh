b2a9dd6512ae606b81f11231efc79ef6850eafe3
Maria Coitinho <brasildu@gmail.com>
2023-04-20 01:20:51 -0500

    nice

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-1idxex6e7sabt/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-1idxex6e7sabt/ExoticCarsRaceApp.war /usr/local/tomcat9/webapps/ExoticCarsRaceApp.war
 
 
 # Ensure the ownership permissions are correct.
