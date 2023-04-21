d1940e7b6db9697bdeb3066f2d7a201301b5d402
Maria Coitinho <brasildu@gmail.com>
2023-04-21 03:04:32 -0500

    update!

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -xe
 
-  # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install
+# Build the Ruby on Rails application
+cd exotic/backed
+bundle install
+bundle exec rake assets:precompile RAILS_ENV=production
 
-
+# Build the React application
+cd ../frontend
+npm install
+npm run build
--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-1idxex6e7sabt/ExoticCarsRaceApp.war /usr/local/tomcat9/webapps/ExoticCarsRaceApp.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-1idxex6e7sabt/Exotic.war/usr/local/tomcat9/webapps/Exotic.war
 
 
 # Ensure the ownership permissions are correct.
