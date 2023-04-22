96bb5572313f9d4427d29a5d85455217a1f75a33
Maria Coitinho <brasildu@gmail.com>
2023-04-22 18:23:02 -0500

    oi

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -e
+
 cd $GITHUB_WORKSPACE/exotic/backend
+sudo apt-get update
+sudo apt-get install -y ruby-full
+gem install bundler
 bundle install
-bundle exec rails assets:precompile RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake assets:precompile RAILS_ENV=production
+
 cd $GITHUB_WORKSPACE/exotic/frontend
+sudo apt-get update
+sudo apt-get install -y nodejs npm
 npm install
 npm run build
